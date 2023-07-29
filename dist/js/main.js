f90993293e6966776fc05a020fa1ff72a1b2eaf1
Roeland <Frederik.Roeland@wolterskluwer.com>
2023-07-29 10:01:32 +0200

    menu button java script and rotate

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+// Selectors
+const menuButton = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('nav-item');
+
+// Set initial state of menu
+let showMenu = false;
+
+menuButton.addEventListener('click',toggleMenu);
+
+function toggleMenu() {
+    // menu is not shown
+    if(!showMenu) {
+        menuButton.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // set menu state
+        showMenu = true;
+    // menu is shown    
+    } else {
+        menuButton.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set menu state
+        showMenu = false;
+ 
+    }
+}
