6f126fba4312a4e0196eb82fb929b610cb6acf52
Roeland <Frederik.Roeland@wolterskluwer.com>
2023-08-03 17:26:19 +0200

    added dark/light theme

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,11 +4,35 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const themeButton = document.querySelector('.toggle-theme');
+const html = document.querySelector('html');
 
-// Set initial state of menu
+// Set initial state of menu and theme
 let showMenu = false;
+let darkTheme = (JSON.parse(localStorage.getItem('darkTheme') !== null)) 
+    ? JSON.parse(localStorage.getItem('darkTheme'))  
+    : true ;
 
+// on page load, apply selected theme
+if(themeButton) {
+    initTheme();
+};
+
+//event listeners
 menuButton.addEventListener('click',toggleMenu);
+themeButton.addEventListener('click',switchTheme);
+
+function initTheme() {
+    //console.log(localStorage.getItem('darkTheme'));
+    let lightThemeSelected = (localStorage.getItem('darkTheme') !== null 
+    && JSON.parse(localStorage.getItem('darkTheme')) === false);
+    //console.log(darkThemeSelected);
+    if(lightThemeSelected) {
+        html.classList.add('light'); 
+        themeButton.classList.remove('fa-sun');
+        themeButton.classList.add('fa-moon');
+    } 
+}
 
 function toggleMenu() {
     // menu is not shown
@@ -33,4 +57,23 @@ function toggleMenu() {
         showMenu = false;
  
     }
+}
+
+function switchTheme() {
+    if(darkTheme) {
+        html.classList.add('light'); 
+        
+        themeButton.classList.remove('fa-sun');
+        themeButton.classList.add('fa-moon');
+        darkTheme = false;
+        localStorage.setItem('darkTheme', false );
+    } else {
+        html.classList.remove('light');
+        
+        themeButton.classList.remove('fa-moon');
+        themeButton.classList.add('fa-sun');
+        darkTheme = true;
+        localStorage.setItem('darkTheme', true );
+    }
+
 }
